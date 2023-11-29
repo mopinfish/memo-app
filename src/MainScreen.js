3968eff8c3e8ee17924133dae9e8670962c8d002
OtsukaNoboru <mopinfish@gmail.com>
2023-11-29 22:28:12 +0900

    add compose screen

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
-import { List } from 'react-native-paper';
+import { List, FAB } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 import format from 'date-fns/format';
-
-const memos = [
-  {
-    text: "メモメモメモ",
-    createdAt: 1585574700000, // 2020.03.30 22:25
-  },
-  {
-    text: "メモメモメモ",
-    createdAt: 1585567500000, // 2020.03.30 20:25
-  },
-  {
-    text: "長いメモメモメモメモメモメモメモメモメモメモメモメモメモメモ",
-    createdAt: 1585459500000, // 2020.03.29 14:25
-  },
-  {
-    text: "メモメモメモ",
-    createdAt: 1585369500000, // 2020.03.28 13:25
-  },
-  {
-    text: "メモメモメモ",
-    createdAt: 1585275900000, // 2020.03.27 11:25
-  },
-];
+import { loadAll } from './store';
 
 export const MainScreen = () => {
+  const navigation = useNavigation();
+  const [memos, setMemos] = useState([]);
+
+  useEffect(() => {
+    const initialize = async () => {
+      const newMemos = await loadAll();
+      setMemos(newMemos);
+    };
+
+    const unsubscribe = navigation.addListener('focus', initialize);
+
+    return unsubscribe;
+  }, [navigation]);
+
+  const onPressAdd = () => {
+    navigation.navigate('Compose');
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -43,6 +40,15 @@ export const MainScreen = () => {
             descriptionStyle={{ textAlign: 'right' }}
           />
         )}
+      />
+      <FAB
+        style={{
+          position: 'absolute',
+          right: 16,
+          bottom: 16,
+        }}
+        icon="plus"
+        onPress={onPressAdd}
       />
     </View>
   );
